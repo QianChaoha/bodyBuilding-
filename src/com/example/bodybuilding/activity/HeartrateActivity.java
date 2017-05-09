c7e608e9faa120d10e3a65e8119d5f76c1cb0fa4
1501814877@qq.com <1501814877@qq.com>
2017-05-09 17:00:38 +0800

    0509

---
+++ src/com/example/bodybuilding/activity/HeartrateActivity.java
@@ -0,0 +1,24 @@
+package com.example.bodybuilding.activity;
+
+import com.example.bodybuilding.R;
+import com.example.bodybuilding.base.BaseActivity;
+
+/**
+ * Created by cqian on 2017/5/9.
+ */
+public class HeartrateActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void getData() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_heart_rate;
+    }
+}
